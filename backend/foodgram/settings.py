259f230825f61f62e655e474fd609cea9e69cf94
Kseniia Nivnia <knivnja@gmail.com>
2022-11-17 12:43:09 +0000

    workflow49

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -129,7 +129,7 @@ USE_L10N = True
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
